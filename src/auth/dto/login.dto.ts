2166601d6ac33e62f32007b94cbb932be2038cb1
SuinLee21 <suin7473@naver.com>
2025-05-08 03:39:24 +0900

    feat: Add validation rules

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,16 @@
+import { IsNotEmpty, IsString, Length, Matches } from 'class-validator';
+import { Regex } from '../common/guards/regex.constant';
+
 export class LoginDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(4, 20)
+  @Matches(Regex.ID)
   id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(8, 20)
+  @Matches(Regex.PW)
   pw: string;
 }
--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,5 +1,22 @@
+import { IsString, IsNotEmpty, Length, Matches } from 'class-validator';
+import { Regex } from '../common/guards/regex.constant';
+
 export class SignupDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(4, 20)
+  @Matches(Regex.ID)
   id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(8, 20)
+  @Matches(Regex.PW)
   pw: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 6)
+  @Matches(Regex.NICKNAME)
   nickname: string;
 }
