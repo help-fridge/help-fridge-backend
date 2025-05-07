05c6d886911509ec74bb5b0f7c6dd2b2ee0c90c7
SuinLee21 <suin7473@naver.com>
2025-05-08 02:35:19 +0900

    refactor: Update condition statement logic

--- src/api/fridge/common/pipes/validate-fridge-sort.pipe.ts
+++ src/api/fridge/common/pipes/validate-fridge-sort.pipe.ts
@@ -4,20 +4,22 @@ import { FridgeSort } from '../../enums/fridge-sort.enum';
 /**
  * 냉장고 정렬 검증 파이프
  * - 요청된 sort 값이 FridgeSort 열거형에 포함되어 있는지 검증
- *
- * 유효하지 않은 sort 값인 경우 null return
  */
 @Injectable()
 export class ValidateFridgeSortPipe implements PipeTransform {
-  transform(value: string): FridgeSort | null {
+  transform(value: string): string {
     const sort = Object.values(FridgeSort).find(
       (p) => p.toLowerCase() === value.toLowerCase(),
     );
 
-    if (!sort) {
-      return null;
+    if (sort === FridgeSort.NAME_ASC) {
+      return 'fd.name ASC';
+    } else if (sort === FridgeSort.EXPIREIN) {
+      return `"expireIn" ASC`;
+    } else if (sort === FridgeSort.ADDED_ASC) {
+      return 'fd.added_at ASC';
     }
 
-    return sort;
+    return 'fd.name ASC';
   }
 }
