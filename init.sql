c93cdd8251cb9a78a1b06cc1d4b1ee9d0f772697
SuinLee21 <suin7473@naver.com>
2025-05-10 02:59:59 +0900

    refactor: Rename columns and adjust constraints for consistency

--- init.sql
+++ init.sql
@@ -6,19 +6,19 @@ CREATE DATABASE help_fridge OWNER help_fridge_admin;
 
 CREATE TABLE category_tb
 (
-  code                 int     NOT NULL,
-  name                 varchar NOT NULL UNIQUE,
-  parent_category_code int    ,
+  code       char(4)     NOT NULL,
+  name       varchar(50) NOT NULL,
+  expiration int         NOT NULL,
   PRIMARY KEY (code)
 );
 
 CREATE TABLE food_history_tb
 (
   idx        int                      NOT NULL GENERATED ALWAYS AS IDENTITY,
-  food_idx   int                      NOT NULL,
+  food_id    varchar(7)               NOT NULL,
   user_idx   int                      NOT NULL,
   reason_idx int                      NOT NULL,
-  amount     decimal                  NOT NULL,
+  amount     int                      NOT NULL,
   added_at   timestamp with time zone NOT NULL,
   created_at timestamp with time zone NOT NULL DEFAULT NOW(),
   PRIMARY KEY (idx)
@@ -26,59 +26,69 @@ CREATE TABLE food_history_tb
 
 CREATE TABLE food_tb
 (
-  idx           int     NOT NULL GENERATED ALWAYS AS IDENTITY,
-  category_code int     NOT NULL,
-  unit_idx      int     NOT NULL,
-  name          varchar NOT NULL UNIQUE,
+  id            varchar(7)  NOT NULL,
+  name          varchar(50) NOT NULL,
+  category_code char(4)     NOT NULL,
+  unit_idx      int         NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE fridge_tb
+(
+  idx         int                      NOT NULL GENERATED ALWAYS AS IDENTITY,
+  food_id     varchar(7)               NOT NULL,
+  user_idx    int                      NOT NULL,
+  storage_idx int                      NOT NULL,
+  amount      int                      NOT NULL,
+  added_at    timestamp with time zone NOT NULL DEFAULT NOW(),
+  expired_at  timestamp with time zone NOT NULL,
+  updated_at  timestamp with time zone NOT NULL DEFAULT NOW(),
   PRIMARY KEY (idx)
 );
 
 CREATE TABLE reason_tb
 (
-  idx  int     NOT NULL GENERATED ALWAYS AS IDENTITY,
-  name varchar NOT NULL UNIQUE,
+  idx  int         NOT NULL GENERATED ALWAYS AS IDENTITY,
+  name varchar(10) NOT NULL UNIQUE,
   PRIMARY KEY (idx)
 );
 
-CREATE TABLE recipe_ingredient_tb
+CREATE TABLE recipe_food_tb
 (
-  food_idx   int     NOT NULL,
-  recipe_idx int     NOT NULL,
-  amount     decimal NOT NULL,
-  PRIMARY KEY (food_idx, recipe_idx)
+  recipe_id int        NOT NULL,
+  food_id   varchar(7) NOT NULL,
+  PRIMARY KEY (recipe_id, food_id)
 );
 
 CREATE TABLE recipe_tb
 (
-  idx  int     NOT NULL GENERATED ALWAYS AS IDENTITY,
-  name varchar NOT NULL UNIQUE,
-  PRIMARY KEY (idx)
+  id     int         NOT NULL,
+  name   varchar(50) NOT NULL,
+  amount varchar(50),
+  time   varchar(30),
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE fridge_tb
+CREATE TABLE storage_tb
 (
-  idx        int                      NOT NULL GENERATED ALWAYS AS IDENTITY,
-  food_idx   int                      NOT NULL,
-  user_idx   int                      NOT NULL,
-  amount     decimal                  NOT NULL,
-  added_at   timestamp with time zone NOT NULL DEFAULT NOW(),
-  expired_at timestamp with time zone NOT NULL,
+  idx  int        NOT NULL GENERATED ALWAYS AS IDENTITY,
+  name varchar(6) NOT NULL,
   PRIMARY KEY (idx)
 );
 
 CREATE TABLE unit_tb
 (
   idx  int     NOT NULL GENERATED ALWAYS AS IDENTITY,
-  name varchar NOT NULL UNIQUE,
+  name char(3) NOT NULL UNIQUE,
   PRIMARY KEY (idx)
 );
 
 CREATE TABLE user_tb
 (
-  idx  int     NOT NULL GENERATED ALWAYS AS IDENTITY,
-  id   varchar NOT NULL UNIQUE,
-  pw   varchar NOT NULL,
-  nickname varchar NOT NULL,
+  idx      int          NOT NULL GENERATED ALWAYS AS IDENTITY,
+  id       varchar(20)  NOT NULL UNIQUE,
+  pw       varchar(100) NOT NULL,
+  nickname varchar(6)   NOT NULL,
   PRIMARY KEY (idx)
 );
 
@@ -89,18 +99,18 @@ ALTER TABLE fridge_tb
 
 ALTER TABLE fridge_tb
   ADD CONSTRAINT FK_food_tb_TO_fridge_tb
-    FOREIGN KEY (food_idx)
-    REFERENCES food_tb (idx);
+    FOREIGN KEY (food_id)
+    REFERENCES food_tb (id);
 
-ALTER TABLE recipe_ingredient_tb
-  ADD CONSTRAINT FK_food_tb_TO_recipe_ingredient_tb
-    FOREIGN KEY (food_idx)
-    REFERENCES food_tb (idx);
+ALTER TABLE recipe_food_tb
+  ADD CONSTRAINT FK_food_tb_TO_recipe_food_tb
+    FOREIGN KEY (food_id)
+    REFERENCES food_tb (id);
 
-ALTER TABLE recipe_ingredient_tb
-  ADD CONSTRAINT FK_recipe_tb_TO_recipe_ingredient_tb
-    FOREIGN KEY (recipe_idx)
-    REFERENCES recipe_tb (idx);
+ALTER TABLE recipe_food_tb
+  ADD CONSTRAINT FK_recipe_tb_TO_recipe_food_tb
+    FOREIGN KEY (recipe_id)
+    REFERENCES recipe_tb (id);
 
 ALTER TABLE food_tb
   ADD CONSTRAINT FK_category_tb_TO_food_tb
@@ -114,8 +124,8 @@ ALTER TABLE food_tb
 
 ALTER TABLE food_history_tb
   ADD CONSTRAINT FK_food_tb_TO_food_history_tb
-    FOREIGN KEY (food_idx)
-    REFERENCES food_tb (idx);
+    FOREIGN KEY (food_id)
+    REFERENCES food_tb (id);
 
 ALTER TABLE food_history_tb
   ADD CONSTRAINT FK_user_tb_TO_food_history_tb
@@ -127,7 +137,7 @@ ALTER TABLE food_history_tb
     FOREIGN KEY (reason_idx)
     REFERENCES reason_tb (idx);
 
-ALTER TABLE category_tb
-  ADD CONSTRAINT FK_category_tb_TO_category_tb
-    FOREIGN KEY (parent_category_code)
-    REFERENCES category_tb (code);
+ALTER TABLE fridge_tb
+  ADD CONSTRAINT FK_storage_tb_TO_fridge_tb
+    FOREIGN KEY (storage_idx)
+    REFERENCES storage_tb (idx);
