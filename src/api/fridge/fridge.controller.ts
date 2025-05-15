91244828369ae6c3e1fe1b5c3735279fbfd3b6ff
jochongs <joch2712@naver.com>
2025-05-16 00:56:44 +0900

    fix: Update wrong import path

--- src/api/fridge/fridge.controller.ts
+++ src/api/fridge/fridge.controller.ts
@@ -11,7 +11,6 @@ import {
 } from '@nestjs/common';
 import { FridgeService } from './fridge.service';
 import { User } from 'src/common/decorator/user.decorator';
-import { AuthGuard } from 'src/auth/common/guards/auth.guard';
 import { Sort } from './common/decorators/fridge-sort.decorator';
 import { CreateFridgeListDto } from './dto/create-fridge-list.dto';
 import { DeleteFridgeDto } from './dto/delete-fridge.dto';
@@ -21,6 +20,7 @@ import { ApiQuery } from '@nestjs/swagger';
 import { SelectAllFridge } from './type/select-all-fridge.type';
 import { selectFridgeToBeConsumed } from './type/select-fridge-to-be-consumed.type';
 import { Type } from './common/decorators/storage-type.decorator';
+import { AuthGuard } from 'src/api/auth/common/guards/auth.guard';
 
 @Controller('fridge')
 export class FridgeController {
