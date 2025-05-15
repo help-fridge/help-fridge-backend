f8b17bc4a0f22d49c30405caf8be8db3121b7e7b
jochongs <joch2712@naver.com>
2025-05-16 01:26:51 +0900

    feat: Implement FridgeV2Service methods

--- src/api/fridge/fridge-v2.service.ts
+++ src/api/fridge/fridge-v2.service.ts
@@ -1,6 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { FridgeEntity } from 'src/api/fridge/entity/fridge.entity';
+import { FridgeV2Repository } from 'src/api/fridge/fridge-v2.repository';
+import { CreateFridgeInput } from 'src/api/fridge/input-v2/create-fridge.input';
+import { GetFridgeAllInput } from 'src/api/fridge/input-v2/get-fridge-all.input';
 
 @Injectable()
 export class FridgeV2Service {
-  constructor() {}
+  constructor(private readonly fridgeRepository: FridgeV2Repository) {}
+
+  public async getFridgeByIdx(idx: number): Promise<FridgeEntity | null> {
+    const fridge = await this.fridgeRepository.selectFridgeByIdx(idx);
+
+    return fridge && FridgeEntity.from(fridge);
+  }
+
+  public async getFridgeAll(input: GetFridgeAllInput): Promise<FridgeEntity[]> {
+    return (await this.fridgeRepository.selectFridgeByAll(input)).map(
+      FridgeEntity.from,
+    );
+  }
+
+  public async createFridge(
+    userIdx: number,
+    input: CreateFridgeInput,
+  ): Promise<FridgeEntity> {
+    const fridge = await this.fridgeRepository.insertFridge(userIdx, input);
+
+    return FridgeEntity.from(fridge);
+  }
+
+  public async updateFridge(
+    fridgeIdx: number,
+    input: CreateFridgeInput,
+  ): Promise<void> {
+    await this.fridgeRepository.updateFridgeByIdx(fridgeIdx, input);
+  }
+
+  public async deleteFridge(idx: number): Promise<void> {
+    await this.fridgeRepository.deleteFridgeByIdx(idx);
+  }
 }
