48a3d56926a66ecd15e977afc7aad96241a89938
jochongs <joch2712@naver.com>
2025-05-16 17:54:34 +0900

    fix: Fix method type of FridgeHistoryService class

--- src/api/fridge-history/fridge-history.service.ts
+++ src/api/fridge-history/fridge-history.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { FridgeHistoryRepository } from 'src/api/fridge-history/fridge-history.repository';
+import { CreateFridgeHistoryInput } from 'src/api/fridge-history/input/create-fridge-history.input';
+import { UpdateFridgeHistoryInput } from 'src/api/fridge-history/input/update-fridge-history.input';
 
 @Injectable()
 export class FridgeHistoryService {
@@ -7,19 +9,34 @@ export class FridgeHistoryService {
     private readonly fridgeHistoryRepository: FridgeHistoryRepository,
   ) {}
 
+  /**
+   * Entity가 없으므로 사용하지 마십시오.
+   *
+   * @deprecated
+   */
   public async getFridgeHistoryByIdx(idx: number) {
     return await this.fridgeHistoryRepository.selectFridgeHistoryByIdx(idx);
   }
 
+  /**
+   * Entity가 없으므로 사용하지 마십시오.
+   *
+   * @deprecated
+   */
   public async getFridgeHistoryByAll() {
     return await this.fridgeHistoryRepository.selectFridgeHistoryByAll();
   }
 
-  public async createFridgeHistory(input: any) {
-    return await this.fridgeHistoryRepository.insertFridgeHistory(input);
+  public async createFridgeHistory(
+    input: CreateFridgeHistoryInput,
+  ): Promise<void> {
+    await this.fridgeHistoryRepository.insertFridgeHistory(input);
   }
 
-  public async updateFridgeHistoryByIdx(idx: number, input: any) {
+  public async updateFridgeHistoryByIdx(
+    idx: number,
+    input: UpdateFridgeHistoryInput,
+  ) {
     return await this.fridgeHistoryRepository.updateFridgeHistoryByIdx(
       idx,
       input,
