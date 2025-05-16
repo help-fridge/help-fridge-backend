0bea97eb14a247dbff96f62cb1f32be429aa75b0
jochongs <joch2712@naver.com>
2025-05-16 17:53:23 +0900

    feat: Implement FridgehistoryService

--- src/api/fridge-history/fridge-history.service.ts
+++ src/api/fridge-history/fridge-history.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { FridgeHistoryRepository } from 'src/api/fridge-history/fridge-history.repository';
 
 @Injectable()
-export class FridgeHistoryService {}
+export class FridgeHistoryService {
+  constructor(
+    private readonly fridgeHistoryRepository: FridgeHistoryRepository,
+  ) {}
+
+  public async getFridgeHistoryByIdx(idx: number) {
+    return await this.fridgeHistoryRepository.selectFridgeHistoryByIdx(idx);
+  }
+
+  public async getFridgeHistoryByAll() {
+    return await this.fridgeHistoryRepository.selectFridgeHistoryByAll();
+  }
+
+  public async createFridgeHistory(input: any) {
+    return await this.fridgeHistoryRepository.insertFridgeHistory(input);
+  }
+
+  public async updateFridgeHistoryByIdx(idx: number, input: any) {
+    return await this.fridgeHistoryRepository.updateFridgeHistoryByIdx(
+      idx,
+      input,
+    );
+  }
+
+  public async deleteFridgeHistoryByIdx(idx: number) {
+    return await this.fridgeHistoryRepository.deleteFridgeHistoryByIdx(idx);
+  }
+}
